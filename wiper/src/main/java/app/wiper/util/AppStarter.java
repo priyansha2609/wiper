477207e906e65cefe65c954ce9b591a6fd3d00cd
Nitesh Pal Singh <niteshpalsingh@Niteshs-MacBook-Pro.local>
2018-05-17 01:51:57 +0530

    fixing spring war context

--- wiper/src/main/java/app/wiper/util/AppStarter.java
+++ wiper/src/main/java/app/wiper/util/AppStarter.java
@@ -1,17 +1,23 @@
 package app.wiper.util;
 
-import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @MapperScan("app.wiper.mapper")
 @SpringBootApplication(scanBasePackages = {"app.wiper"})
-public class AppStarter {
+public class AppStarter extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppStarter.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppStarter.class);
+    }
 
 }
